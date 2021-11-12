84ecaf9664725d0f36a39469d84c80e0573618b2
offetuoso <offetuoso@gmail.com>
2021-11-13 00:02:40 +0900

    2021-11-13

---
+++ example/0.WebPack/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'
+import example from './example.vue';
+new Vue().$mount('#root');
+
---
+++ example/0.WebPack/webpack.config.js
@@ -0,0 +1,23 @@
+//const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const { VueLoaderPlugin } = require('vue-loader')
+const path = require('path');
+
+module.exports = { //웹패킹을 할때 모듈을 사용
+    entry : {
+        app : path.join(__dirname,'main.js') //하나로 합쳐질 파일의 이름 스크립트 및 그외 파일들 rules로 js로 변환해서 통합 저장됨
+
+    },
+    module : { //모듈의 핵심 어떻게 합칠것인지 
+        rules : [{
+            test : /\.vue$/,
+            loader :  'vue-loader', 
+        }],
+    },
+    plugins : [
+        new VueLoaderPlugin()
+    ],
+    output : {
+        filename : '[name].js',//'app.js',
+        path : path.join(__dirname, 'dist'),
+    }
+};
