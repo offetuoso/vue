53b7ec3eb16b60e4d38d032f22656b378e6363ed
offetuoso <offetuoso@gmail.com>
2021-11-26 23:43:50 +0900

    2021-11-26

--- example/8_틱택토/TdComponent.vue
+++ example/8_틱택토/TdComponent.vue
@@ -15,6 +15,27 @@ export default {
             console.log(this.rowIndex, this.cellIndex);
             //this.$root.$data.tableData[this.rowIndex][this.cellIndex] = this.$root.$data.turn;
 
+            // → ↘ ↓ ↙ ← ↖ ↑ ↗ 순서
+            var dx = [1, 2, 2, 1, 0, 0, -1, -2, -1, -2, 0, 0, 1, 2];
+            var dy = [0, 0, -2, -1, -1, -2, -1, -2, 0, 0, -1, -2, 1, 2];
+
+            for (var i = 0; i < dx.length; i++) {
+                if (
+                    this.cellIndex + dx[i] <= 2 &&
+                    this.rowIndex + dy[i] <= 2 &&
+                    this.cellIndex + dx[i] >= 0 &&
+                    this.rowIndex + dy[i] >= 0
+                ) {
+                    console.log(
+                        this.rowIndex + dy[i],
+                        this.cellIndex + dx[i],
+                        this.$root.$data.tableData[this.rowIndex + dy[i]][
+                            this.cellIndex + dx[i]
+                        ]
+                    );
+                }
+            }
+
             this.$set(
                 this.$root.$data.tableData[this.rowIndex],
                 this.cellIndex,
